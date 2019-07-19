71d9eaa6a4bdc9e2cab5749918f4d5d60fd7989a
JupiterDev <ilyaklyus@gmail.com>
2019-07-19 15:24:01 +0300

    pre-finish commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,51 +8,107 @@ class App extends Component {
     this.state = {
       rows: [[], [], []],
       total: {
-        ctns: '',
-        totQty: '',
-        volume: '',
-        empty1: '',
-        amoutRmb: '',
-        GW: '',
-        NW: ''
+        ctns: 0,
+        totQty: 0,
+        volume: 0,
+        empty: 0,
+        amoutRmb: 0,
+        GW: 0,
+        NW: 0
       }
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.tableRows = this.tableRows.bind(this);
     this.totalRow = this.totalRow.bind(this);
+    this.parseData = this.parseData.bind(this);
+  }
+
+  // ПРЕОБРАЗОВЫВАЕМ STATE В МАССИВ ОБЪЕКТОВ ДЛЯ ЛУЧШЕЙ ЧИТАЕМОСТИ (КАЖДЫЙ ОБЪЕКТ - СТРОКА НАШЕЙ ТАБЛИЦЫ)
+  parseData() {
+    const { rows, total } = this.state;
+    const headTitles = [
+      'Int code',
+      'Our model',
+      'Factory model',
+      'goods name',
+      'ctn#',
+      'ctns',
+      'qty/ctn',
+      'tot.qty',
+      'dimension',
+      'volume',
+      'ctns * dimension',
+      'amout(rmb)',
+      'volume/pic',
+      'unit',
+      'G.W/ctn',
+      'ctns * G.W/ctn',
+      'G.W',
+      'N.W/ctn',
+      'N.W',
+      'box weight',
+      'ready date'
+    ];
+
+    const rowsArr = rows.map(array => {
+      const obj = {};
+      for (let i = 0; i < 21; i++) {
+        obj[headTitles[i]] = array[i];
+      }
+      return obj;
+    });
+
+    const data = JSON.stringify({ items: rowsArr, totals: total });
+
+    console.log(data);
   }
 
   // ОБРАБОТЧИК ВВОДИМЫХ ДАННЫХ В ПОЛЯ ТАБЛИЦЫ
   handleChange(e) {
     const { rows, total } = this.state;
-    const rowIndex = e.target.getAttribute('data-row');
-    const columnIndex = e.target.getAttribute('data-column');
+    const rowIndex = parseInt(e.target.getAttribute('data-row'), 10);
+    const columnIndex = parseInt(e.target.getAttribute('data-column'), 10);
 
-    const calc = () =>
-      rows.reduce((prevValue, curValue) => prevValue + curValue[5], 0);
-
-    this.setState(prevState => ({
-      ...prevState,
-      rows: rows.map((item, index) => {
-        if (index !== rowIndex) {
-          return item;
+    // функция подсчета total-ов
+    const calc = x => {
+      let value = 0;
+      for (let i = 0; i < rows.length; i++) {
+        if (rows[i][x] !== undefined && !isNaN(rows[i][x])) {
+          value += parseInt(rows[i][x], 10);
         }
-        return item.map((itemj, indexj) =>
-          indexj === columnIndex ? e.target.value : itemj
-        );
+      }
+      return value;
+    };
+    this.setState({
+      ...this.state,
+      rows: rows.map((array, index) => {
+        for (let i = 0; i < 21; i++) {
+          if (rowIndex === index && i === columnIndex) {
+            array[i] = parseInt(e.target.value, 10);
+          }
+          array[7] = array[5] * array[6];
+          array[10] = array[5] * array[8];
+          array[15] = array[5] * array[14];
+        }
+        return array;
       }),
       total: {
         ...total,
-        ctns: calc
+        ctns: calc(5),
+        totQty: calc(7),
+        volume: calc(9),
+        empty: calc(10),
+        amoutRmb: calc(11),
+        GW: calc(16),
+        NW: calc(18)
       }
-    }));
+    });
   }
 
   // ВОЗВРАЩАЕМ РЕДАКТИРУЕМЫЕ СТРОКИ ТАБЛИЦЫ
   tableRows() {
     const { rows } = this.state;
-
     return rows.map((row, index) => {
       const arrayOfRowCells = [];
       for (let i = 0; i < 21; i += 1) {
@@ -62,8 +118,11 @@ class App extends Component {
             <td key={`${index}-${i}`} data-row={index} data-column={i}>
               <input
                 type="text"
-                value={row[i]}
-                onChange={this.handleChange}
+                value={row[i] ? row[i] : null}
+                onChange={event => {
+                  event.persist();
+                  this.handleChange(event);
+                }}
                 data-row={index}
                 data-column={i}
               />
@@ -74,14 +133,15 @@ class App extends Component {
           arrayOfRowCells.push(
             <td
               className="calculated-field"
-              value={row[i]}
               key={`${index}-${i}`}
               data-row={index}
-              data-column={i}></td>
+              data-column={i}>
+              {row[i] ? row[i] : null}
+            </td>
           );
         }
       }
-      return <tr key={Math.random()}>{arrayOfRowCells}</tr>;
+      return <tr key={index}>{arrayOfRowCells}</tr>;
     });
   }
 
@@ -93,18 +153,18 @@ class App extends Component {
         <td colSpan="5">TOTAL:</td>
         <td className="calculated-field">{total.ctns}</td>
         <td></td>
-        <td className="calculated-field">1,44</td>
+        <td className="calculated-field">{total.totQty}</td>
         <td></td>
-        <td className="calculated-field">1,44</td>
-        <td className="calculated-field">1,44</td>
-        <td className="calculated-field">1,44</td>
-        <td></td>
-        <td></td>
+        <td className="calculated-field">{total.volume}</td>
+        <td className="calculated-field">{total.empty}</td>
+        <td className="calculated-field">{total.amoutRmb}</td>
         <td></td>
         <td></td>
-        <td className="calculated-field">1,44</td>
         <td></td>
-        <td className="calculated-field">1,44</td>
+        <td></td>
+        <td className="calculated-field">{total.GW}</td>
+        <td></td>
+        <td className="calculated-field">{total.NW}</td>
         <td></td>
         <td></td>
       </tr>
@@ -121,6 +181,9 @@ class App extends Component {
             {this.totalRow()} {/* ПОСЛЕДНЯЯ СТРОКА С ПОДСЧЕТАМИ */}
           </tbody>
         </table>
+        <button type="button" onClick={this.parseData}>
+          Спарсить данные
+        </button>
       </div>
     );
   }
